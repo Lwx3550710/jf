f88b436dc6618d84709fbbb7418d5a2ea72e493a
liwx <903096129@qq.com>
2019-08-05 19:49:10 +0800

    对接优惠券

--- pages/coupons/index.js
+++ pages/coupons/index.js
@@ -15,6 +15,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    that = this;
     this.getCoupon();
   },
 
@@ -26,6 +27,9 @@ Page({
       },
       success: function (res) {
         console.log(res);
+        that.setData({
+          couponData: res.list,
+        })
       },
     })
   },
